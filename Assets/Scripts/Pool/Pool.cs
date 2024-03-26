3875158100f77f5df42e068b7439aa332b503fdb
Alnur (アルヌール) <82401346+xwvwww@users.noreply.github.com>
2024-03-26 16:59:51 +0600

    Adding Pool

---
+++ Assets/Scripts/Pool/Pool.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pool
+{
+    public string name;
+    public int size;
+    public bool resize;
+    public List<GameObject> Objects;
+
+    public Pool(string name, int size, bool resize)
+    {
+        this.name = name;
+        this.size = size;
+        this.resize = resize;
+        Objects = new List<GameObject>();
+    }
+
+
+
+}
--- Assets/Scripts/Pool/PoolManager.cs
+++ Assets/Scripts/Pool/PoolManager.cs
@@ -2,17 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PoolManager : MonoBehaviour
+public class PoolManager
 {
     
-    void Start()
-    {
-        
-    }
-
-    
-    void Update()
-    {
-        
-    }
 }
