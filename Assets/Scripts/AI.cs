c266defab617efca59f4fd0089b4960f51c0c4aa
Alnur (アルヌール) <82401346+xwvwww@users.noreply.github.com>
2024-03-28 17:10:16 +0500

    Testing AI WayPoint

--- Assets/Scripts/AI.cs
+++ Assets/Scripts/AI.cs
@@ -5,19 +5,33 @@ using UnityEngine.AI;
 
 public class AI : MonoBehaviour
 {
+    [SerializeField] private Transform[] _points;
     [SerializeField] private Transform _target;
+
+
     private NavMeshAgent _agent;
+    private Transform _currentPoint;
 
 
 
     private void Awake()
     {
         _agent = GetComponent<NavMeshAgent>();
+
+
+    }
+
+    private void Start()
+    {
+        int index = Random.Range(0, _points.Length);
+        _currentPoint = _points[index];
     }
 
     void Update()
     {
-        _agent.SetDestination(_target.position);
+        _agent.SetDestination(_currentPoint.position);
+
+
     }
 
 }
